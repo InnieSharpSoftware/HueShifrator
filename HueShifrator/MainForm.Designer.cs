3efd5935ad4d8e21992c6df52980696d3d0025e9
InnieSharpSoftware <116297277+InnieSharpSoftware@users.noreply.github.com>
2022-11-19 16:39:04 +0300

    fv
    Supports:
    Russian
    English
    -!,._+=0123456789#

---
+++ HueShifrator/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿/*
+By InnieSharp(ix4/i#)
+*/
+namespace HueShifrator
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Red;
+			this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.Location = new System.Drawing.Point(0, 0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(39, 332);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Lime;
+			this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button2.Location = new System.Drawing.Point(414, 0);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(39, 332);
+			this.button2.TabIndex = 1;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox1.Location = new System.Drawing.Point(39, 0);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(375, 332);
+			this.richTextBox1.TabIndex = 2;
+			this.richTextBox1.Text = "Red - Encrypt, Green - Decrypt";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(453, 332);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "HueShifrator";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ HueShifrator/MainForm.cs
@@ -0,0 +1,196 @@
+﻿/*
+By InnieSharp(ix4/i#)
+*/
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace HueShifrator
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		Dictionary<char, char> chars = new Dictionary<char, char>
+		{
+			{ 'q', 'Å' },
+			{ 'w', 'Á' },
+			{ 'e', 'Ð' },
+			{ 'r', 'ã' },
+			{ 't', 'ñ' },
+			{ 'y', 'Ü' },
+			{ 'u', '¼' },
+			{ 'i', 'ß' },
+			{ 'o', '÷' },
+			{ 'p', 'ú' },
+			{ 'a', 'þ' },
+			{ 's', 'Ø' },
+			{ 'd', 'µ' },
+			{ 'f', '¥' },
+			{ 'g', 'Ï' },
+			{ 'h', 'ð' },
+			{ 'j', 'ÿ' },
+			{ 'k', '▢' },
+			{ 'l', '◮' },
+			{ 'z', '▰' },
+			{ 'x', '◯' },
+			{ 'c', '◹' },
+			{ 'v', '◘' },
+			{ 'b', '▙' },
+			{ 'n', '✠' },
+			{ 'm', '☰' },
+			{ 'Q', '☑' },
+			{ 'W', '☈' },
+			{ 'E', '★' },
+			{ 'R', '☢' },
+			{ 'T', '❆' },
+			{ 'Y', '✡' },
+			{ 'U', '❤' },
+			{ 'I', '✂' },
+			{ 'O', '✇' },
+			{ 'P', '➱' },
+			{ 'A', '❐' },
+			{ 'S', '❖' },
+			{ 'D', '⑰' },
+			{ 'F', '⑫' },
+			{ 'G', '⑦' },
+			{ 'H', '⓿' },
+			{ 'J', '❻' },
+			{ 'K', '▅' },
+			{ 'L', '░' },
+			{ 'Z', '╳' },
+			{ 'X', '╪' },
+			{ 'C', '┆' },
+			{ 'V', '┈' },
+			{ 'B', 'ᴂ' },
+			{ 'N', 'ῲ' },
+			{ 'M', 'ᶈ' },
+			{ ',', 'ᴖ' },
+			{ '.', 'ᴗ' },
+			{ '!', 'ᴙ' },
+			{ '?', 'ᴞ' },
+			{ 'й', 'ℂ'},
+			{ 'ц', 'ℋ'},
+			{ 'у', '℈'},
+			{ 'к', 'ℍ'},
+			{ 'е', 'ℐ'},
+			{ 'н', '℔'},
+			{ 'г', '℗'},
+			{ 'ш', 'ℙ'},
+			{ 'щ', 'ℛ'},
+			{ 'з', '℠'},
+			{ 'х', '℡'},
+			{ 'ъ', '℣'},
+			{ 'ф', '℥'},
+			{ 'ы', 'Ω'},
+			{ 'в', '℧'},
+			{ 'а', '℩'},
+			{ 'п', 'ℬ'},
+			{ 'р', 'ℱ'},
+			{ 'о', '℻'},
+			{ 'л', '⅓'},
+			{ 'д', '⅔'},
+			{ 'ж', '⅕'},
+			{ 'э', '⅖'},
+			{ 'я', '⅗'},
+			{ 'ч', 'ℽ'},
+			{ 'с', '⅀'},
+			{ 'м', '⅁'},
+			{ 'и', '⅂'},
+			{ 'т', '⅄'},
+			{ 'ь', 'ⅇ'},
+			{ 'б', '⅊'},
+			{ 'ю', '⅋'},
+			{ 'Й', '⅍'},
+			{ 'Ц', 'ⅎ'},
+			{ 'У', '∀'},
+			{ 'К', '∄'},
+			{ 'Е', '∉'},
+			{ 'Н', '∎'},
+			{ 'Г', '∐'},
+			{ 'Ш', '∓'},
+			{ 'Щ', '√'},
+			{ 'З', '∜'},
+			{ 'Х', '∞'},
+			{ 'Ъ', '∠'},
+			{ 'Ф', '∢'},
+			{ 'Ы', '∤'},
+			{ 'В', '∬'},
+			{ 'А', '∰'},
+			{ 'П', '∴'},
+			{ 'Р', '∹'},
+			{ 'О', '∾'},
+			{ 'Л', '≅'},
+			{ 'Д', '≈'},
+			{ 'Ж', '≊'},
+			{ 'Э', '≔'},
+			{ 'Я', '≝'},
+			{ 'Ч', '≠'},
+			{ 'С', '≨'},
+			{ 'М', '≫'},
+			{ 'И', '≪'},
+			{ 'Т', '≭'},
+			{ 'Ь', '≲'},
+			{ 'Б', '≸'},
+			{ 'Ю', '≽'},
+			{ '-', '⋢'},
+			{ '=', '⋥'},
+			{ '+', '⋪'},
+			{ '_', '⋮'},
+			{ '0', '⋳'},
+			{ '1', '⌘'},
+			{ '2', '⌗'},
+			{ '3', '⋈'},
+			{ '4', '⇵'},
+			{ '5', '⇱'},
+			{ '6', '⊶'},
+			{ '7', '⌇'},
+			{ '8', '⌧'},
+			{ '9', '♡'},
+			{ '#', '⋰'},
+		};
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			string next = "";
+			foreach(char c in richTextBox1.Text)
+			{
+				if(chars.ContainsKey(c))
+					next += chars[c].ToString();
+				else
+					next += c.ToString();
+			}
+			richTextBox1.Text = next;
+			// var myKey = types.FirstOrDefault(x => x.Value == "one").Key;
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			string next = "";
+			foreach(char c in richTextBox1.Text)
+			{
+				if(chars.ContainsValue(c))
+				{
+					var myKey = chars.FirstOrDefault(x => x.Value == c).Key;
+					next += myKey.ToString();
+				}
+				else
+					next += c.ToString();
+			}
+			richTextBox1.Text = next;
+		}
+	}
+}
